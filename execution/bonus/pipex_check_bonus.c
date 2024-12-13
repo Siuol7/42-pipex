b942c7e9ba9bc520375488274d01a7d6f9f64b6e
Siuol7 <nguyencaotan03072000@gmail.com>
2024-12-13 05:36:24 +0200

    Update access_check

--- execution/bonus/pipex_check_bonus.c
+++ execution/bonus/pipex_check_bonus.c
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 03:25:16 by caonguye          #+#    #+#             */
-/*   Updated: 2024/12/13 02:24:11 by caonguye         ###   ########.fr       */
+/*   Updated: 2024/12/13 05:27:27 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,36 +30,17 @@ int	path_envp_check(char **envp)
 	return (0);
 }
 
-// int	access_cmd_path(char *cmd_path)
-// {
-// 	struct stat	stat_path;
-
-// 	if (stat(cmd_path, &stat_path) != -1)
-// 	{
-// 		if (S_ISDIR(stat_path.st_mode))
-// 			error_isdir(cmd_path);
-// 	}
-// 	if (access(*cmd, F_OK) == 0)
-// 	{
-// 		if (access(*cmd, X_OK) == 0)
-// 			return (0);
-// 		else
-// 			return (126);
-// 	}
-// 	return (127);
-// }
-
 int	access_check(char **cmd)
 {
 	struct stat	stat_path;
 
-	if (stat(*cmd, &stat_path) != -1)
-	{
-		if (S_ISDIR(stat_path.st_mode))
-			return (1);
-	}
 	if (access(*cmd, F_OK) == 0)
 	{
+		if (stat(*cmd, &stat_path) != -1)
+		{
+			if (S_ISDIR(stat_path.st_mode))
+				return (1);
+		}
 		if (access(*cmd, X_OK) == 0)
 			return (0);
 		else
@@ -67,3 +48,4 @@ int	access_check(char **cmd)
 	}
 	return (127);
 }
+
--- execution/mandatory/pipex_check.c
+++ execution/mandatory/pipex_check.c
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 03:24:58 by caonguye          #+#    #+#             */
-/*   Updated: 2024/12/13 02:25:03 by caonguye         ###   ########.fr       */
+/*   Updated: 2024/12/13 05:27:03 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,13 @@ int	access_check(char **cmd)
 {
 	struct stat	stat_path;
 
-	if (stat(*cmd, &stat_path) != -1)
-	{
-		if (S_ISDIR(stat_path.st_mode))
-			return (1);
-	}
 	if (access(*cmd, F_OK) == 0)
 	{
+		if (stat(*cmd, &stat_path) != -1)
+		{
+			if (S_ISDIR(stat_path.st_mode))
+				return (1);
+		}
 		if (access(*cmd, X_OK) == 0)
 			return (0);
 		else
